05dce29c14012c57097127fa9c511afea234b0a8
romansu <romansu@checkpoint.com>
2020-01-07 16:59:35 +0200

    NetworkPriceSection, SmallBusinessesPriceSection

--- src/common/itemCard/priceSections/SmallBusinessesPriceSection.js
+++ src/common/itemCard/priceSections/SmallBusinessesPriceSection.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+const Container = styled.div`  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;`;
+
+const PriceText = styled.span`  
+  font-size: 30px;
+  text-align: center;`;
+
+const SubPriceText = styled.span`  
+  text-align: center;
+  color: #858991;
+  font-size: 14px;
+  margin-top: 30px;`;
 
 const SmallBusinessesPriceSection = (props) => {
 
 	return (
-		<div>
-			<p className="price-font">59 $</p>
-			<p>1 PC/YEAR & 1 Mobile/Year</p>
-		</div>
+		<Container>
+			<PriceText>$2,700</PriceText>
+			<SubPriceText>(Starting at)</SubPriceText>
+		</Container>
 	);
 };
 
