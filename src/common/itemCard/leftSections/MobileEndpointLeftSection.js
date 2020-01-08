ad4a0bb16754a472f9e76fdbc3ea98f5af393470
romansu <romansu@checkpoint.com>
2020-01-08 16:32:27 +0200

    MobileEndpointLeftSection

--- src/common/itemCard/leftSections/MobileEndpointLeftSection.js
+++ src/common/itemCard/leftSections/MobileEndpointLeftSection.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import { colors } from '../../../config/constants';
+import Image from '../../Image';
+import verified from '../../../assets/verified@2x.png';
+
 
 const Container = styled.div`
   display: flex;
@@ -10,31 +13,67 @@ const Container = styled.div`
 const DescriptionSection = styled.div`
   width: 60%;
   height: 100%;
-  padding: 0 15px;`;
+  padding: 15px 15px;`;
 
 const PackagesSection = styled.div`
-  display: flex;
   width: 40%;
-  height: 100%;`;
+  height: 100%;
+  margin-top: 48px;
+`;
 
 const Title = styled.p`
   font-size: 18px;
-  font-weight: bold;`;
+  font-weight: bold;
+  margin: 0 0 5px 0;
+`;
 
-const Description = styled.p`
+const Description = styled.span`
     font-size: 14px;`;
 
 const AdditionalLabel = styled.span`
 color: ${colors.checkPointPink};
 `;
 
+const UlTitle = styled.div`
+	color: ${colors.textLightGray};
+	font-weight: lighter;
+	font-size: 14px;
+	margin-bottom: 15px;
+`;
+
+const FeatureContainer = styled.div`
+	height: auto;
+	margin-bottom: 15px;
+	display: flex;
+	align-items: center;
+`;
+
+const FeatureText = styled.div`
+	font-weight: bold;
+	font-size: 14px;
+	display: inline-block;
+	margin-left: 6px;
+`;
+
 const MobileEndpointLeftSection = (props) => {
 
-	// renderPackageList = (packages) => {
-	// 	return packeges.map((p) => {
-	//		
-	// 	})
-	// }
+	const mockPackages = [
+		{	id: 1, feature: 'SandBlast Agent Advanced.' },
+		{	id: 2, feature: 'SandBlast Mobile Per Device' },
+		{	id: 3, feature: 'Two 6500 gateways' },
+		{	id: 4, feature: '450 Gbps using 52 gateways' },
+	];
+
+	const renderPackageList = (packages) => {
+		return packages.map((p) => {
+			return (
+				<FeatureContainer key={p.id}>
+					<Image path={verified} width={15}/>
+					<FeatureText>{p.feature}</FeatureText>
+				</FeatureContainer>
+			);
+		});
+	};
 
 	return (
 		<Container>
@@ -49,7 +88,8 @@ const MobileEndpointLeftSection = (props) => {
 				</Description>
 			</DescriptionSection>
 			<PackagesSection>
-				
+				<UlTitle>INCLUDED PACKAGES:</UlTitle>
+				{renderPackageList(mockPackages)}
 			</PackagesSection>
 		</Container>
 	);
