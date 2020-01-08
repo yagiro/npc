a878bde52fae8d83ba82fe43495e3ee2e984ada9
romansu <romansu@checkpoint.com>
2020-01-08 10:39:55 +0200

    dropdown implemented (react-select)

--- src/common/itemCard/priceSections/ManagementPriceSection.js
+++ src/common/itemCard/priceSections/ManagementPriceSection.js
@@ -1,5 +1,7 @@
 import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
+import Select from 'react-select';
+
 
 const Container = styled.div`
   display: flex;
@@ -7,48 +9,92 @@ const Container = styled.div`
   flex-direction: column;
   flex-wrap: wrap;
   justify-content: space-between;`;
-
 const SelectContainer = styled.div`
   margin-top: 15px;`;
-
 const BottomPriceContainer = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;`;
-
 const SelectTitle = styled.span`  
   text-align: center;
   color: #858991;
   font-size: 14px;`;
-
 const PriceText = styled.span`  
   font-size: 18px;`;
-
 const PriceLabel = styled.span`  
   color: #6B6F81;
   font-size: 14px; `;
+//react-select option
+const DomainWord = styled.span`  
+  font-weight: bold;
+ `;
+const OptionContainer = styled.div`
+  display: flex;
+  font-size: 14px;
+  align-items: center;
+  justify-content: space-between;`;
+const OptionPrice = styled.div`
+  font-size: 12px;
+  color: #999999;
+  text-align: right;
+  margin-left: 35px;`;
+
 
 const ManagementPriceSection = (props) => {
 
-	const [value, setValue] = useState('200.000');
-	const handleChange = useCallback((e) => {
-		setValue(e.target.value);
-	}, [setValue]);
+	const options = [
+		{ value: '100', label: '100 Domainas', price: '$200,000' },
+		{ value: '50', label: '50 Domainas', price: '$185,000' },
+		{ value: '25', label: '25 Domainas', price: '$155,000' },
+		{ value: '10', label: '10 Domainas', price: '$121,000' }
+	];
+
+	const [option, setOption] = useState(options[0]);
+	const handleChange = useCallback((option) => {
+		setOption(option);
+	}, [setOption]);
+
+
+	//option render function
+	const formatOptionLabel = ({ value, label, price }) => (
+		<OptionContainer>
+			<div>
+				<span>{value}</span>
+				<DomainWord> Domains</DomainWord>
+			</div>
+			<OptionPrice>
+				{price}
+			</OptionPrice>
+		</OptionContainer>
+	);
 
 	return (
 		<Container>
 			<SelectContainer>
 				<SelectTitle>Number of Domain:</SelectTitle>
-				<select value={value} onChange={handleChange}>
-					<option value="200.000">100 Domaines 200.000$</option>
-					<option value="300.000">300 Domaines 300.000$</option>
-					<option value="400.000">400 Domaines 400.000$</option>
-					<option value="500.000">500 Domaines 500.000$</option>
-				</select>
+				<Select
+					defaultValue={options[0]}
+					formatOptionLabel={formatOptionLabel}
+					options={options}
+					value={option}
+					onChange={handleChange}
+					components={{
+						IndicatorSeparator: () => null
+					}}
+					theme={theme => ({
+						...theme,
+						borderRadius: 3,
+						colors: {
+							...theme.colors,
+							primary25: '#E856851A',
+							primary: 'rgba(236,64,122,0.5)',
+						},
+					})}
+				/>
 			</SelectContainer>
 			<BottomPriceContainer>
 				<PriceLabel>Total price: </PriceLabel>
-				<PriceText>{value}$</PriceText>
+				<PriceText>{option ? option.price : 0}$</PriceText>
 			</BottomPriceContainer>
 		</Container>
 	);
