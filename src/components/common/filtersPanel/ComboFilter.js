e09266316d18c5bb34b8bdb78726f53f7a0c162b
romansu <romansu@checkpoint.com>
2020-02-02 16:50:44 +0200

    - removed useEffect from ComboFilter - show options in ComboFilter only if dropdown chosen

--- src/components/common/filtersPanel/ComboFilter.js
+++ src/components/common/filtersPanel/ComboFilter.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import Select from 'react-select';
@@ -9,6 +9,7 @@ import FiltersOption from './FiltersOption';
 
 const Container = styled.div`
 	display: block;
+	min-width: 120px;
 `;
 
 // styles for react-select
@@ -23,12 +24,6 @@ const ComboFilter = ({ filterId, title, data, chosenFilters, onChange }) => {
 
 	const options = data.multipleOptionsFilter.options;
 	const dropDownOptions = data.dropdownFilter.options;
-
-	//componentDidMount : we need to set Default value to select
-	useEffect(() => {
-		handleDropdownChange(dropDownOptions[0], chosenFilters);
-	}, [ ]);
-
 
 	const isChecked = (chosenFilters, filterId, value) => {
 		return !!(chosenFilters[filterId] && chosenFilters[filterId].value.has(value));
@@ -50,17 +45,17 @@ const ComboFilter = ({ filterId, title, data, chosenFilters, onChange }) => {
 	const handleDropdownChange = useCallback((dropdownOption, chosenFilters) => {
 
 		const prevFilter = { ...chosenFilters[filterId] };
-		const multipleOptions = prevFilter.value || new Set([]);
+		const multipleOptions = prevFilter.value || new Set();
 		const updatedFilter = { ...prevFilter, dropdownFilter: dropdownOption, value: multipleOptions, filterId };
 		
 		onChange({ ...chosenFilters, [filterId]: updatedFilter });
-	}, [filterId, onChange]);
+	}, [ filterId, onChange ]);
 	
 	return (
 		<Container>
 			<Title color={ colors.textLightGray } margin="0 0 15px 0">{ title }</Title>
 			{/*// render list of filter-options*/}
-
+			
 			<Select
 				styles={ customStyles }
 				value={ chosenFilters[filterId] ? chosenFilters[filterId].dropdownFilter : null }
@@ -81,7 +76,9 @@ const ComboFilter = ({ filterId, title, data, chosenFilters, onChange }) => {
 					},
 				}) }
 			/>
-			
+
+			{/*we show options only if dropdown chosen*/}
+			{ chosenFilters[filterId] &&
 			<div>
 				{ options.map((option) => {
 					const checked = isChecked(chosenFilters, option.filterId, option.value);
@@ -96,9 +93,9 @@ const ComboFilter = ({ filterId, title, data, chosenFilters, onChange }) => {
 							chosenFilters={ chosenFilters }
 						/>
 					);
-				})
-				}
+				}) }
 			</div>
+			}
 		</Container>
 
 	);
